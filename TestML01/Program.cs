9bd73f3a2f2676ad3b97b913950e8beaa00f11db
egoquat <egohim@gmail.com>
2018-05-10 15:35:37 +0900

    New Addition

---
+++ TestML01/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.ML;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+
+namespace TestML01
+{
+    class Program
+    {
+        // STEP 1: Define your data structures
+
+        // IrisData is used to provide training data, and as 
+        // input for prediction operations
+        // - First 4 properties are inputs/features used to predict the label
+        // - Label is what you are predicting, and is only set when training
+        public class IrisData
+        {
+            [Column("0")]
+            public float SepalLength;
+
+            [Column("1")]
+            public float SepalWidth;
+
+            [Column("2")]
+            public float PetalLength;
+
+            [Column("3")]
+            public float PetalWidth;
+
+            [Column("4")]
+            [ColumnName("Label")]
+            public string Label;
+        }
+
+        // IrisPrediction is the result returned from prediction operations
+        public class IrisPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public string PredictedLabels;
+        }
+
+        static void Main(string[] args)
+        {
+            // STEP 2: Create a pipeline and load your data
+            LearningPipeline pipeline = new LearningPipeline();
+
+            string dataPath = "iris-data.txt";
+            pipeline.Add(new TextLoader<IrisData>(dataPath, separator: ","));
+
+            pipeline.Add(new Dictionarizer("Label"));
+
+            pipeline.Add(new ColumnConcatenator("Features", "SepalLength", "SepalWidth", "PetalLength", "PetalWidth"));
+
+            pipeline.Add(new StochasticDualCoordinateAscentClassifier());
+
+            pipeline.Add(new PredictedLabelColumnOriginalValueConverter() { PredictedLabelColumn = "PredictedLabel" });
+
+            var model = pipeline.Train<IrisData, IrisPrediction>();
+
+            var prediction = model.Predict(
+                new IrisData()
+                {
+                    SepalLength = 3.3f,
+                    SepalWidth = 1.6f,
+                    PetalLength = 0.2f,
+                    PetalWidth = 5.1f,
+                });
+
+            Console.WriteLine($"Predicted flower type is: {prediction.PredictedLabels}");
+        }
+    }
+}
---
+++ TestML02/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using Microsoft.ML.Models;
+using Microsoft.ML.Runtime;
+using Microsoft.ML.Runtime.Api;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.ML;
+
+namespace TestML02
+{
+    class Program
+    {
+        public class SentimentData
+        {
+            [Column(ordinal: "0")]
+            public string SentimentText;
+            [Column(ordinal: "1", name: "Label")]
+            public float Sentiment;
+        }
+
+        public class SentimentPrediction
+        {
+            [ColumnName("PredictedLabel")]
+            public bool Sentiment = false;
+        }
+
+        const string _dataPath = @"..\..\..\data\sentiment labelled sentences\imdb_labelled.txt";
+        const string _testDataPath = @"..\..\..\data\sentiment labelled sentences\yelp_labelled.txt";
+
+        public static PredictionModel<SentimentData, SentimentPrediction> TrainAndPredict()
+        {
+            string pathCurrent = System.IO.Directory.GetCurrentDirectory();
+
+            var pipeline = new LearningPipeline();
+            pipeline.Add(new TextLoader<SentimentData>(_dataPath, useHeader: false, separator: "tab"));
+            pipeline.Add(new TextFeaturizer("Features", "SentimentText"));
+            pipeline.Add(new FastTreeBinaryClassifier() { NumLeaves = 5, NumTrees = 5, MinDocumentsInLeafs = 2 });
+
+            PredictionModel<SentimentData, SentimentPrediction> model = null;
+            try
+            {
+                model = pipeline.Train<SentimentData, SentimentPrediction>();
+
+                IEnumerable<SentimentData> sentiments = new[]
+                {
+                    new SentimentData
+                    {
+                        SentimentText = "Contoso's 11 is a wonderful experience",
+                        Sentiment = 0
+                    },
+                    new SentimentData
+                    {
+                        SentimentText = "The acting in this movie is very bad",
+                        Sentiment = 0
+                    },
+                    new SentimentData
+                    {
+                        SentimentText = "Joe versus the Volcano Coffee Company is a great film.",
+                        Sentiment = 0
+                    },
+                    new SentimentData
+                    {
+                        SentimentText = "horrible.",
+                        Sentiment = 0
+                    },
+                    new SentimentData
+                    {
+                        SentimentText = "oh my godness. wonderful.",
+                        Sentiment = 0
+                    },
+                    new SentimentData
+                    {
+                        SentimentText = "Contoso's 11 is not an ordinary expirence. just fine.",
+                        Sentiment = 0
+                    },
+                    new SentimentData
+                    {
+                        SentimentText = "It's based on an personal condition.",
+                        Sentiment = 0
+                    }
+                };
+
+                IEnumerable<SentimentPrediction> predictions = model.Predict(sentiments);
+                var sentimentsAndPredictions = sentiments.Zip(predictions, (sentiment, prediction) => (sentiment, prediction));
+
+                Console.WriteLine();
+                Console.WriteLine("Result Sentiment Predictions >>>>>>>>>>>>>>>>>>");
+                Console.WriteLine("---------------------");
+                foreach ((SentimentData, SentimentPrediction) item in sentimentsAndPredictions)
+                {
+                    SentimentData sentiment = item.Item1;
+                    SentimentPrediction prediction = item.Item2;
+                    Console.WriteLine($"Sentiment: {sentiment.SentimentText} | Prediction: {(prediction.Sentiment ? "Positive" : "Negative")}");
+                }
+                Console.WriteLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("error:" + e.Message);
+            }
+
+            return model;
+        }
+
+        public static void Evaluate(PredictionModel<SentimentData, SentimentPrediction> model)
+        {
+            var testData = new TextLoader<SentimentData>(_testDataPath, useHeader: false, separator: "tab");
+            var evaluator = new BinaryClassificationEvaluator();
+            BinaryClassificationMetrics metrics = evaluator.Evaluate(model, testData);
+
+            Console.WriteLine();
+            Console.WriteLine("PredictionModel quality metrics evaluation");
+            Console.WriteLine("------------------------------------------");
+            Console.WriteLine($"Accuracy: {metrics.Accuracy:P2}");
+            Console.WriteLine($"Auc: {metrics.Auc:P2}");
+            Console.WriteLine($"F1Score: {metrics.F1Score:P2}");
+        }
+
+        static void Main(string[] args)
+        {
+            var model = TrainAndPredict();
+
+            Evaluate(model);
+        }
+    }
+}
