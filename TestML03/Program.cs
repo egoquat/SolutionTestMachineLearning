85948e8f1bbeb2416f26210eda8fc7b29b557f76
egoquat <egohim@gmail.com>
2018-05-11 06:28:23 +0900

    Regression Sample

--- TestML03/Program.cs
+++ TestML03/Program.cs
@@ -8,9 +8,35 @@ namespace TestML03
 {
     class Program
     {
+        public static readonly string DataPath = @"..\..\..\Data\taxi-fare-train.csv";
+        public static readonly string TestDataPath = @"..\..\..\Data\taxi-fare-test.csv";
+        public static readonly string ModelPath = @"..\..\..\Models\Model.zip";
+
         static void Main(string[] args)
         {
-            
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = Train();
+
+        }
+
+        public static PredictionModel<TaxiTrip, TaxiTripFarePrediction> Train()
+        {
+            var pipeline = new LearningPipeline
+            {
+                new TextLoader<TaxiTrip>(DataPath, useHeader: true, separator: ","),
+                new ColumnCopier(("fare_amount", "Label")),
+                new CategoricalOneHotVectorizer("vendor_id",
+                                             "rate_code",
+                                             "payment_type"),
+                new ColumnConcatenator("Features",
+                                                "vendor_id",
+                                                "rate_code",
+                                                "passenger_count",
+                                                "trip_distance",
+                                                "payment_type"),
+                new FastTreeRegressor()
+            };
+
+            PredictionModel<TaxiTrip, TaxiTripFarePrediction> model = pipeline.Train<TaxiTrip, TaxiTripFarePrediction>();
         }
     }
 }
--- TestML03/TaxiTrip.cs
+++ TestML03/TaxiTrip.cs
@@ -1,15 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Microsoft.ML.Models;
+using Microsoft.ML.Trainers;
+using Microsoft.ML.Transforms;
+using Microsoft.ML;
 using Microsoft.ML.Runtime.Api;
 
 namespace TestML03
 {
+    class TaxiTripFarePrediction
+    {
+        float fare_amount;
+    }
+
     class TaxiTrip
     {
-        const string DataPath = @"..\..\..\Data\taxi-fare-train.csv";
-        const string TestDataPath = @"..\..\..\Data\taxi-fare-test.csv";
-        const string ModelPath = @"..\..\..\Models\Model.zip";
-
+        LearningPipeline Pipeline = new LearningPipeline
+        {
+            new TextLoader<TaxiTrip>(Program.DataPath, useHeader: true, separator: ","),
+            new ColumnCopier(("fare_amount", "Label")),
+            new CategoricalOneHotVectorizer("vendor_id",
+                                         "rate_code",
+                                         "payment_type"),
+            new ColumnConcatenator("Features",
+                                            "vendor_id",
+                                            "rate_code",
+                                            "passenger_count",
+                                            "trip_distance",
+                                            "payment_type"),
+            new FastTreeRegressor()
+        };
     }
 }
